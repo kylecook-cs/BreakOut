2b11b6174a8b984fffc78561d4a1fa28b2bb4c5e
Kyle Cook <75640815+kylecook-cs@users.noreply.github.com>
2023-03-22 17:53:09 -0500

    Updated Classes
    Updated the scenes and added some methods to Player class. Testing in main

--- src/com/skillstorm/game/BreakOut.java
+++ src/com/skillstorm/game/BreakOut.java
@@ -1,7 +1,6 @@
 package com.skillstorm.game;
 
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -10,103 +9,66 @@ public class BreakOut {
 	static Scanner in = new Scanner(System.in);
 	
 
-	public static void main(String[] args) throws InterruptedException {
-		
+	public static void main(String[] args) {
+		playAgain();
+	}
+	
+	public static void playAgain() {
 		Random randomGen = new Random();
 		ArrayList<Integer> scenes = new ArrayList<Integer>();
-		
-		println("*****BREAKOUT!*****");
-		println("\nHere you are sitting in back of the police car, questioning your life decisions.");
-		println("You are wondering how you got in this position but no matter how much you replay what you have done");
-		println("you know that it doesn't matter now. You are headed to your new home.");
-		
-		println("\nCop: You thought you'd get away with it huh? Sike! Now look at you, you're headed to prison.");
-		println("Cop: Good Luck, the Java State Prison is no joke.");
-		println("Cop: But you know what they say, don't do the crime if you can't do the time");
-		
-		println("\nYou arrive at the prison and are escorted to the processing unit.\n");
-		println("************************************************");
-		
-		Player inmate = admin();
+		boolean playAgain;
+		String again = "";
+		do {
+			println("*****BREAKOUT!*****");
+			
+			println("\nHere you are sitting in back of the police car, questioning your life decisions.");
+			println("You are wondering how you got in this position but no matter how much you replay "
+					+ "\nwhat you have done you know that it doesn't matter now. You are headed to your "
+					+ "\nnew home.");
+			
+			println("\nCOP: You thought you'd get away with it huh? Sike! Now look at you, you're headed"
+					+ " to prison.");
+			println("COP: Good Luck, the Java State Prison is no joke.");
+			println("COP: But you know what they say, don't do the crime if you can't do the time");
+			
+			println("\nYou arrive at the prison and are escorted to the processing unit.\n");
+			println("***********************************************************************************");
+			println("\n***** ADMIN *****");
+			
+			Scenes gameScene = new Scenes();
+			Player inmate = Scenes.admin();
 	
-		println("Admin: Processing is now complete. Enjoy your stay Prisoner " + inmate.getName());
-		println("\nDay1 Night1....\n Day2 Night2....\n  Day3 Night3...\n");
-		
-		println("After spending 3 days in the Java State Prison, you realize this isn't the place for you");
-		println("Of course you believe you shouldn't be there in the first place, since we \"we all know your innocent\".");
-		println("But understanding you only have one real option to get out, and that's to BREAKOUT.");
-		println("You decide tomorrow will be the day you'll escape from the Java State Prison.");
-		
-		Scenes gameScene = new Scenes();
-		println("********** NEXT DAY **********");
-		while (scenes.size() < 5) {
-			int random = randomGen.nextInt(5);
-			if (!scenes.contains(random)) {
-				scenes.add(random);
-			}
-		}
-		for (int num : scenes) {
-			if (num == 0) {
-				gameScene.cell();
-			} else if (num == 1) {
-				gameScene.kitchen();
-			} else if (num == 2) {
-				gameScene.dayroom();
-			} else if (num == 3) {
-				gameScene.laundry();
-			} else if (num == 4) {
-				gameScene.yard();
-			}
-		}	
-	}
+//			while (scenes.size() < 5) {
+//				int random = randomGen.nextInt(5);
+//				if (!scenes.contains(random)) {
+//					scenes.add(random);
+//				}
+//			}
+//			scenes.add(0);
+//			scenes.add(1);
+//			scenes.add(2);
+//			scenes.add(3);
+//			scenes.add(4);
+			
+			
+			gameScene.yard(inmate);
+			
+			do {
+				print("Would you like to play again? (Y/N): ");
+				again = in.nextLine();
+				if (again.equalsIgnoreCase("y")) {
+				playAgain = true;
+				} else {
+					playAgain = false;
+				}
+			} while (!again.equalsIgnoreCase("y") && !again.equalsIgnoreCase("n"));
+		} while (playAgain);
+	}	
 	public static void print(Object string) {
 		System.out.print(string);
 	}
 	public static void println(Object string) {
 		System.out.println(string);
 	}
-	public static int getRandom(int max) {
-		return (int) (Math.random()*max); 
-		} 
-	public static int getRandomInteger(int maximum, int minimum) {
-		return ((int) (Math.random()*(maximum - minimum))) + minimum;
-		}
-	public static Player admin() {
-		
-		String username;
-		int securityLevel;
-		
-		println("Admin: Welcome to the Java State Prison");
-		println("Admin: This is the processing unit, I will need just a couple pieces of information from you.");
-		do {
-			print("Admin: May I have your name?: ");
-			username = in.nextLine();
-		} while (username.trim().equals(""));
-		do {
-			println("\nAdmin: Thank you, Prisoner " + username);
-			try {
-				print("Admin: Now can you tell me what your security level is "
-						+ "(1: minimum, 2: average, 3: maximum): ");
-				securityLevel = in.nextInt();
-			} catch(InputMismatchException e) {
-				securityLevel = 100;
-				in.nextLine();
-			}
-		} while (securityLevel != 1 && securityLevel != 2 && securityLevel != 3);
-		
-		if (securityLevel == 1) {
-			println("\nAdmin: Level 1 security... Hmmmm interesting, you must have just forgot a ; somewhere, so I'll put in your file"
-					+ " that you fall under the soft catergory.");
-			Player user = new Player(username, securityLevel, 5, 2, 5);
-			return user;
-		} else if (securityLevel == 2) {
-			println("\nAdmin: Level 2 security you say, you must have used the wrong data type? I'll be sure to note that.");
-			Player user = new Player(username, securityLevel, 5, 3, 3);	
-			return user;
-		} else {
-			println("\nAdmin: Level 3 Yikeees! You must be an infinite looper! Remind me to stay out of your code...");
-			Player user = new Player(username, securityLevel, 10, 5, 1);
-			return user;
-		}
-	}
+	
 }
--- src/com/skillstorm/game/Player.java
+++ src/com/skillstorm/game/Player.java
@@ -1,5 +1,7 @@
 package com.skillstorm.game;
 
+import java.util.ArrayList;
+
 public class Player {
 	
 	private String name;
@@ -7,19 +9,21 @@ public class Player {
 	private int healthPoints;
 	private int strength;
 	private int intellegence;
+	private ArrayList<String> inventory = new ArrayList<String>(5);
 	
 	public Player(String name,
 				 int securityLevel, 
 				 int healthPoints, 
 				 int strength, 
-				 int intellegence) {
+				 int intellegence,
+				 ArrayList<String> inventory) {
 		
 		this.name = name;
 		this.healthPoints = healthPoints;
 		this.securityLevel = securityLevel;
 		this.strength = strength;
 		this.intellegence = intellegence;
-		
+		this.inventory = inventory;
 	}
 
 	public String getName() {
@@ -52,4 +56,31 @@ public class Player {
 	public void setIntellegence(int intellegence) {
 		this.intellegence = intellegence;
 	}
+	public ArrayList<String> getInventory() {
+		return inventory;
+	}
+	public void setInventory(ArrayList<String> inventory, String item) {	
+		this.inventory = inventory;
+		this.inventory.add(item);
+	}
+	public String toString() {
+		String inventoryList = "";
+		for (String inv : inventory) {
+			inventoryList += (inv + " ");
+		}
+		return String.format("============================ PLAYER STATS ============================\n"
+							+ "\nPrisoner %s  "
+							+ "Security Level:%d  "
+							+ "Health:%d  "
+							+ "Stength:%d  "
+							+ "Intelligence:%d  " 
+							+ "Inventory: %s\n",
+							name,
+							securityLevel,
+							healthPoints,
+							strength,
+							intellegence,
+							inventoryList);
+	}	
 }
+
